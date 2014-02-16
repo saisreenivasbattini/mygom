8f18bd22ca146f5e6cac7a1e8107cc58e0c0eb25
sreenivas <srinu0904@gmail.com>
2014-02-16 22:42:17 +0530

    project has been created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mygoms::Application.config.secret_key_base = '77d60b0823f825d8bc3b72c2b308059fe940368d6a20a4c136d1c44bec4fa2d67e4e90c2298cb1ccc48945bb3d0cf19cae490f0fc13efcac8ed5cb5edb5a4ec4'
